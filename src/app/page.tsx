69d086c07cfe01b3184c7da457eba0d67bce1803
l-dokhoianAL <lilit.dokhoian100@gmail.com>
2023-12-05 11:12:15 +0400

    Finalize the project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import FilmContainer from "@/components/film-container";
+
+export default function Home() {
+  return (
+    <div className="flex gap-4">
+      <FilmContainer />
+    </div>
+  )
+}
